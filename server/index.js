73e68e6fbed491ccf67d53e8354654d3eea8dddb
Kamari <kmoore971@gmail.com>
2025-01-20 11:10:48 -0600

    Updated backend for vercel deployment

--- server/index.js
+++ server/index.js
@@ -27,6 +27,11 @@ mongoose.connect(process.env.MONGO_URL);
 // routes
 
 // Start app
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+// app.listen(5000, () => {
+//   console.log("Server started on port 5000");
+// });
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
